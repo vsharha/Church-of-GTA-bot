87023d9fff78b3e7a009d8ffbac353dcd91b1367
Viktor Sharha <wisefearcsgo@gmail.com>
2023-12-10 11:50:34 +0000

    Split trailer
    Added split_vid.py
    Restored trailer functionality

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import csv
 import os
 import random as r
 from datetime import datetime as dt
-# import cv2
 import pytz
 
 import discord
@@ -21,6 +20,7 @@ intents.message_content = True
 intents.members = True
 bot = commands.Bot(command_prefix=commands.when_mentioned_or("pr ", "PR ", "Pr ", "pR "), case_insensitive=True, intents=intents)
 
+
 @bot.event
 async def on_ready():
     # with open("users.csv", "r") as f:
@@ -38,9 +38,10 @@ help_command_values = (
     ("pr help", "Show this message"),
     ("pr pray", "See the current time in some server members' timezones"),
     ("pr gta6", "Get an accurate Trailer 2 release date prediction"),
-    ("pr trailer 1-5", "Get random frames from Trailer 1"),
+    ("pr trailer 1-10", "Get random frames from Trailer 1"),
     ("pr [sam/lucia/jason] 1-10", "Get random pictures of Sam Houser, Lucia or Jason"),
 )
+
 
 class my_help(commands.HelpCommand):
     async def send_bot_help(self, mapping):
@@ -62,6 +63,7 @@ bot.help_command = my_help()
 def add_embed_links(embed):
     embed.add_field(name = "Links", value = "[Add Me to Your Server](https://discord.com/api/oauth2/authorize?client_id=997899986668888155&permissions=0&scope=bot)", inline = False)
 
+
 def get_date(timezone):
     now = dt.now(pytz.timezone(timezone))
     date_str = now.strftime("%dth of %B, %I:%M %p"+(" (%H:%M)" if now.strftime('%H') != now.strftime('%I') else ""))
@@ -74,6 +76,7 @@ def get_date(timezone):
                 "th", "st" if check == 1 else "nd" if check == 2 else "rd")
     return date_str
 
+
 def get_valid_range(num, maxi):
     try:
         num = int(num)
@@ -85,6 +88,7 @@ def get_valid_range(num, maxi):
     if num > maxi:
         return maxi
     return num
+
 
 async def send_random_photo_from_dir(ctx, dir, arg1, maxi=10):
     arg1 = get_valid_range(arg1, maxi)
@@ -99,31 +103,7 @@ async def send_random_photo_from_dir(ctx, dir, arg1, maxi=10):
             text=f"pr {dir} - Let me know if you have any suggestions!")
         
         await ctx.channel.send(file = file, embed = embed)
-    
-# async def send_random_frame(ctx, arg1, vid_file_name):
-#     arg1 = get_valid_range(arg1, 5)
 
-#     vidcap = cv2.VideoCapture(vid_file_name)
-#     n_frames = vidcap.get(cv2.CAP_PROP_FRAME_COUNT)
-#     for i in range(arg1):
-#         random_frame_ind = r.randint(10, n_frames)
-#         # set frame position
-#         vidcap.set(cv2.CAP_PROP_POS_FRAMES, random_frame_ind)
-#         success, image = vidcap.read()
-
-#         file_name = f"{ctx.message.id}.jpg"
-#         if success and not os.path.exists(file_name):
-#             cv2.imwrite(file_name, image)
-#             file = discord.File(file_name)
-
-#             embed = discord.Embed(description=f"**Random frame{f' #{i+1}' if arg1 > 1 else ''}:**",
-#                                   color=discord.Color.purple())
-#             embed.set_image(url = "attachment://" + file.filename)
-#             embed.set_footer(
-#                 text="pr trailer - Let me know if you have any suggestions!")
-
-#             await ctx.channel.send(file = file, embed = embed)
-#             os.remove(file_name)
 
 @bot.event
 async def on_message(message):
@@ -137,9 +117,9 @@ async def on_message(message):
     elif bot.user.mentioned_in(message) and ("real" in msg_content or "fake" in msg_content):
         match(r.randint(0,2)):
             case 0:
-                msg = "Real."
+                msg = r.choice("Real.", "Yeah.")
             case 1:
-                msg = "Fake."
+                msg = r.choice("Fake.", "Nah.")
             case 2:
                 msg = f"{r.randint(1,99)}% real."
         await message.channel.send(msg)
@@ -171,7 +151,6 @@ async def pray(ctx):
                     except commands.errors.CommandInvokeError.UnknownTimeZoneError:
                         date = "Error"
                     embed.add_field(name=date, value=member.mention, inline=True)
-                        
                 
     if not is_server_registered:
         embed = discord.Embed(
@@ -184,6 +163,7 @@ async def pray(ctx):
     add_embed_links(embed)
     embed.set_footer(text="pr pray - Let me know if you have any suggestions!")
     await ctx.channel.send(embed=embed)    
+
 
 @bot.command(aliases=['gta7'])
 async def gta6(ctx):
@@ -217,6 +197,7 @@ async def gta6(ctx):
     embed.set_footer(text="pr gta6")
     await ctx.channel.send(embed=embed)
 
+
 # Commands that are based on functions
 @bot.command()
 async def gta(ctx, arg1):
@@ -224,18 +205,22 @@ async def gta(ctx, arg1):
     if arg1 in (6, 7):
         await gta6(ctx)
 
-# @bot.command()
-# async def trailer(ctx, arg1=1):
-#     # await send_random_frame(ctx, arg1, "vid/trailer1.mp4")
-#     await send_random_photo_from_dir(ctx, "trailer", arg1, 5)
+
+@bot.command()
+async def trailer(ctx, arg1=1):
+    # await send_random_frame(ctx, arg1, "vid/trailer1.mp4")
+    await send_random_photo_from_dir(ctx, "trailer", arg1)
+
 
 @bot.command()
 async def lucia(ctx, arg1=1):
     await send_random_photo_from_dir(ctx, "lucia", arg1)
 
+
 @bot.command()
 async def sam(ctx, arg1=1):
     await send_random_photo_from_dir(ctx, "sam", arg1)
+
 
 @bot.command()
 async def jason(ctx, arg1=1):
@@ -245,12 +230,33 @@ async def jason(ctx, arg1=1):
 # RUNNING THE BOT
 
 
-try:
-    bot.run(token)
-except discord.errors.HTTPException as HTTPException:
-    if HTTPException.status == 429:
-        print("The bot is being rate limited, trying to restart")
-        os.system("kill 1")
-        os.system("python restarter.py")
-    else:
-        raise
+bot.run(token)
+
+
+# Requires cv2
+
+
+# async def send_random_frame(ctx, arg1, vid_file_name):
+#     arg1 = get_valid_range(arg1, 5)
+
+#     vidcap = cv2.VideoCapture(vid_file_name)
+#     n_frames = vidcap.get(cv2.CAP_PROP_FRAME_COUNT)
+#     for i in range(arg1):
+#         random_frame_ind = r.randint(10, n_frames)
+#         # set frame position
+#         vidcap.set(cv2.CAP_PROP_POS_FRAMES, random_frame_ind)
+#         success, image = vidcap.read()
+
+#         file_name = f"{ctx.message.id}.jpg"
+#         if success and not os.path.exists(file_name):
+#             cv2.imwrite(file_name, image)
+#             file = discord.File(file_name)
+
+#             embed = discord.Embed(description=f"**Random frame{f' #{i+1}' if arg1 > 1 else ''}:**",
+#                                   color=discord.Color.purple())
+#             embed.set_image(url = "attachment://" + file.filename)
+#             embed.set_footer(
+#                 text="pr trailer - Let me know if you have any suggestions!")
+
+#             await ctx.channel.send(file = file, embed = embed)
+#             os.remove(file_name)
--- restarter.py
+++
@@ -1,5 +0,0 @@
-from time import sleep
-sleep(30)
-
-from os import system
-system("python main.py")
